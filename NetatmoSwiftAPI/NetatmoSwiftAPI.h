20aff036a98ebc3eaf4bcf9e5194c3199d4ab694
James Barrow <baza207@me.com>
2019-10-23 20:28:49 +0200

    Initial Commit

---
+++ NetatmoSwiftAPI/NetatmoSwiftAPI.h
@@ -0,0 +1,19 @@
+//
+//  NetatmoSwiftAPI.h
+//  NetatmoSwiftAPI
+//
+//  Created by James Barrow on 2019-10-23.
+//  Copyright © 2019 Pig on a Hill Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetatmoSwiftAPI.
+FOUNDATION_EXPORT double NetatmoSwiftAPIVersionNumber;
+
+//! Project version string for NetatmoSwiftAPI.
+FOUNDATION_EXPORT const unsigned char NetatmoSwiftAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetatmoSwiftAPI/PublicHeader.h>
+
+
