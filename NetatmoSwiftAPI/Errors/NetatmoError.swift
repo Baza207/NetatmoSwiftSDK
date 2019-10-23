1c34862e0de498db26019b14c132820fdcca917e
James Barrow <baza207@me.com>
2019-10-23 20:47:43 +0200

    Added custom errors

---
+++ NetatmoSwiftAPI/Errors/NetatmoError.swift
@@ -0,0 +1,17 @@
+//
+//  NetatmoError.swift
+//  NetatmoSwiftAPI
+//
+//  Created by James Barrow on 2019-10-23.
+//  Copyright © 2019 Pig on a Hill Productions. All rights reserved.
+//
+
+import Foundation
+
+public enum NetatmoError: Error {
+    case badURL
+    case noData
+    case generalError
+    case noRefreshToken
+    case noAccessToken
+}
