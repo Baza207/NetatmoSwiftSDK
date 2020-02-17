af9174609293cbe488ed0f87cf1e8eaec4fea8ea
James Barrow <baza207@me.com>
2020-02-17 23:00:39 +0100

    Refactored to add login state persistance via keychain

--- NetatmoSwiftAPI/Manager/Authentication.swift
+++ NetatmoSwiftAPI/Manager/Authentication.swift
@@ -12,6 +12,245 @@ public extension NetatmoManager {
     
     // MARK: - Authentication
     
+    func authorizeURL(scope: [AuthScope] = [.readStation]) throws -> URL {
+        
+        self.requestedScope = scope
+        
+        guard var urlComponents = URLComponents(string: "https://api.netatmo.com/oauth2/authorize") else {
+            throw NetatmoError.badURL
+        }
+        
+        if stateUUID == nil {
+            stateUUID = UUID().uuidString
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: clientId),
+            URLQueryItem(name: "redirect_uri", value: redirectURI),
+            URLQueryItem(name: "scope", value: AuthScope.string(from: scope)),
+            URLQueryItem(name: "state", value: stateUUID)
+        ]
+        
+        guard let url = urlComponents.url else {
+            authState = .failed(NetatmoError.badURL)
+            throw NetatmoError.badURL
+        }
+        
+        return url
+    }
+    
+    func authorizationCallback(with url: URL, completed: ((Result<AuthState, Error>) -> Void)? = nil) {
+        
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        
+        guard
+            let state = urlComponents?.queryItems?.first(where: { $0.name == "state" })?.value,
+            let stateUUID = self.stateUUID, state == stateUUID,
+            let code = urlComponents?.queryItems?.first(where: { $0.name == "code" })?.value
+        else {
+            
+            let authState: AuthState = .failed(NetatmoError.noCallbackCode)
+            self.authState = authState
+            completed?(Result.success(authState))
+            return
+        }
+        
+        token(with: code) { [weak self] (result) in
+            
+            let authState: AuthState
+            switch result {
+            case .success:
+                authState = .authorized
+            case .failure(let error):
+                authState = .failed(error)
+            }
+            
+            self?.authState = authState
+            completed?(Result.success(authState))
+        }
+    }
+    
+    // MARK: - Token
+    
+    internal func token(with code: String, completed: @escaping (Result<AuthState, Error>) -> Void) {
+        
+        guard let url = URL(string: "https://api.netatmo.com/oauth2/token") else {
+            completed(Result.failure(NetatmoError.badURL))
+            return
+        }
+        
+        guard let requestedScope = self.requestedScope else {
+            let error = NetatmoError.noScope
+            completed(Result.failure(error))
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        
+        let body = "grant_type=authorization_code&client_id=\(clientId)&client_secret=\(clientSecret)&code=\(code)&redirect_uri=\(redirectURI)&scope=\(AuthScope.string(from: requestedScope))"
+        urlRequest.httpBody = body.data(using: .utf8)!
+        
+        let requestDate = Date()
+        let downloadTask = URLSession.shared.dataTask(with: urlRequest) { [weak self] (data, _, error) in
+            
+            if let error = error {
+                completed(Result.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completed(Result.failure(NetatmoError.noData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let result: AuthResult?
+            do {
+                result = try decoder.decode(AuthResult.self, from: data)
+            } catch {
+                completed(Result.failure(error))
+                return
+            }
+            
+            guard let authResult = result, let self = self else {
+                completed(Result.failure(NetatmoError.generalError))
+                return
+            }
+            
+            self.accessToken = authResult.accessToken
+            self.refreshToken = authResult.refreshToken
+            let expires = authResult.expiresInDate(from: requestDate)
+            self.expires = expires
+            
+            if let stateUUID = self.stateUUID {
+                
+                self.saveStateUUID(stateUUID)
+                
+                let state = OAuthState(accessToken: authResult.accessToken, refreshToken: authResult.refreshToken, expires: expires)
+                try? KeychainPasswordItem(service: NetatmoManager.keychainServiceName, account: stateUUID)
+                    .saveJSON(state)
+            }
+            
+            completed(Result.success(.authorized))
+        }
+        downloadTask.resume()
+    }
+    
+    func refreshToken(_ completed: @escaping (Result<AuthState, Error>) -> Void) {
+        
+        guard let refreshToken = self.refreshToken, refreshToken.isEmpty == false, let expires = self.expires else {
+            completed(Result.failure(NetatmoError.noRefreshToken))
+            return
+        }
+        
+        // If the token has not yet expired, just return success
+        if expires < Date() {
+            completed(Result.success(.authorized))
+        }
+        
+        guard let url = URL(string: "https://api.netatmo.com/oauth2/token") else {
+            completed(Result.failure(NetatmoError.badURL))
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        
+        let body = "grant_type=refresh_token&client_id=\(clientId)&client_secret=\(clientSecret)&refresh_token=\(refreshToken)"
+        urlRequest.httpBody = body.data(using: .utf8)!
+        
+        let requestDate = Date()
+        let downloadTask = URLSession.shared.dataTask(with: urlRequest) { [weak self] (data, _, error) in
+            
+            if let error = error {
+                completed(Result.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completed(Result.failure(NetatmoError.noData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let result: AuthResult?
+            do {
+                result = try decoder.decode(AuthResult.self, from: data)
+            } catch {
+                completed(Result.failure(error))
+                return
+            }
+            
+            guard let authResult = result, let self = self else {
+                completed(Result.failure(NetatmoError.generalError))
+                return
+            }
+            
+            self.accessToken = authResult.accessToken
+            self.refreshToken = authResult.refreshToken
+            let expires = authResult.expiresInDate(from: requestDate)
+            self.expires = expires
+            
+            if let stateUUID = self.stateUUID {
+                
+                self.saveStateUUID(stateUUID)
+                
+                let state = OAuthState(accessToken: authResult.accessToken, refreshToken: authResult.refreshToken, expires: expires)
+                try? KeychainPasswordItem(service: NetatmoManager.keychainServiceName, account: stateUUID)
+                    .saveJSON(state)
+            }
+            
+            completed(Result.success(.authorized))
+        }
+        downloadTask.resume()
+    }
+    
+    // MARK: - Logout
+    
+    func logout() throws {
+        
+        if let stateUUID = self.stateUUID {
+            try KeychainPasswordItem(service: NetatmoManager.keychainServiceName, account: stateUUID)
+                .deleteItem()
+        }
+        
+        removeStateUUID()
+        
+        accessToken = nil
+        refreshToken = nil
+        expires = nil
+        requestedScope = nil
+        stateUUID = nil
+        
+        authState = .unknown
+    }
+    
+    // MARK: - Auth State Changed Listner
+    
+    typealias Listener = UUID
+    
+    func addAuthStateDidChangeListener(_ callback: @escaping ((_ authState: AuthState) -> Void)) -> Listener {
+        
+        // FIXME: Add to queue to avail race conditions
+        let listener = Listener()
+        authStateDidChangeListeners[listener] = callback
+        
+        // Perform callback with current state
+        callback(authState)
+        
+        return listener
+    }
+    
+    func removeAuthStateDidChangeListener(with uuid: Listener) {
+        // FIXME: Add to queue to avail race conditions
+        authStateDidChangeListeners.removeValue(forKey: uuid)
+    }
+    
+}
+
+internal extension NetatmoManager {
+    
     /// Client credentials grant type
     ///
     /// - WARNING: This method should only be used for personnal use and testing purpose.
@@ -23,7 +262,7 @@ public extension NetatmoManager {
     ///   - password: User password
     ///   - scope: Scopes required
     ///   - completed: `AuthResult` with OAuth2 details or an error
-    internal func login(username: String, password: String, scope: [AuthScope] = [.readStation], completed: @escaping (Result<AuthResult, Error>) -> Void) {
+    func login(username: String, password: String, scope: [AuthScope] = [.readStation], completed: @escaping (Result<AuthResult, Error>) -> Void) {
         
         guard let url = URL(string: "https://api.netatmo.com/oauth2/token") else {
             completed(Result.failure(NetatmoError.badURL))
@@ -70,191 +309,6 @@ public extension NetatmoManager {
             completed(Result.success(authResult))
         }
         downloadTask.resume()
-    }
-    
-    func authorizeURL(scope: [AuthScope] = [.readStation]) throws -> URL {
-        
-        // TODO: Chack if a state UUID already exists and check if the user wants to override it
-        guard stateUUID == nil else {
-            authStateDidChangeListeners.values.forEach { $0(.failed(NetatmoError.existingState)) }
-            throw NetatmoError.existingState
-        }
-        
-        guard var urlComponents = URLComponents(string: "https://api.netatmo.com/oauth2/authorize") else {
-            throw NetatmoError.badURL
-        }
-        
-        let stateUUID = UUID().uuidString
-        self.stateUUID = stateUUID
-        self.requestedScope = scope
-        
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: clientId),
-            URLQueryItem(name: "redirect_uri", value: redirectURI),
-            URLQueryItem(name: "scope", value: AuthScope.string(from: scope)),
-            URLQueryItem(name: "state", value: stateUUID)
-        ]
-        
-        guard let url = urlComponents.url else {
-            authStateDidChangeListeners.values.forEach { $0(.failed(NetatmoError.badURL)) }
-            throw NetatmoError.badURL
-        }
-        
-        return url
-    }
-    
-    func authorizationCallback(with url: URL, completed: ((Result<AuthResult, Error>) -> Void)? = nil) {
-        
-        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
-        
-        guard
-            let state = urlComponents?.queryItems?.first(where: { $0.name == "state" })?.value,
-            let stateUUID = self.stateUUID, state == stateUUID,
-            let code = urlComponents?.queryItems?.first(where: { $0.name == "code" })?.value
-        else {
-            authStateDidChangeListeners.values.forEach { $0(.failed(NetatmoError.noCallbackCode)) }
-            return
-        }
-        
-        token(with: code) { [weak self] (result) in
-            
-            switch result {
-            case .success:
-                self?.authStateDidChangeListeners.values.forEach { $0(.authorized) }
-            case .failure(let error):
-                self?.authStateDidChangeListeners.values.forEach { $0(.failed(error)) }
-            }
-        }
-    }
-    
-    internal func token(with code: String, completed: @escaping (Result<AuthResult, Error>) -> Void) {
-        
-        guard let url = URL(string: "https://api.netatmo.com/oauth2/token") else {
-            completed(Result.failure(NetatmoError.badURL))
-            return
-        }
-        
-        guard let requestedScope = self.requestedScope else {
-            authStateDidChangeListeners.values.forEach { $0(.failed(NetatmoError.noScope)) }
-            return
-        }
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-        
-        let body = "grant_type=authorization_code&client_id=\(clientId)&client_secret=\(clientSecret)&code=\(code)&redirect_uri=\(redirectURI)&scope=\(AuthScope.string(from: requestedScope))"
-        urlRequest.httpBody = body.data(using: .utf8)!
-        
-        let requestDate = Date()
-        let downloadTask = URLSession.shared.dataTask(with: urlRequest) { [weak self] (data, _, error) in
-            
-            if let error = error {
-                completed(Result.failure(error))
-                return
-            }
-            
-            guard let data = data else {
-                completed(Result.failure(NetatmoError.noData))
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            let result: AuthResult?
-            do {
-                result = try decoder.decode(AuthResult.self, from: data)
-            } catch {
-                completed(Result.failure(error))
-                return
-            }
-            
-            guard let authResult = result, let self = self else {
-                completed(Result.failure(NetatmoError.generalError))
-                return
-            }
-            
-            self.accessToken = authResult.accessToken
-            self.refreshToken = authResult.refreshToken
-            self.expires = authResult.expiresInDate(from: requestDate)
-            
-            completed(Result.success(authResult))
-        }
-        downloadTask.resume()
-    }
-    
-    func refreshToken(_ completed: @escaping (Result<AuthResult, Error>) -> Void) {
-        
-        guard let refreshToken = self.refreshToken, refreshToken.isEmpty == false, let expires = self.expires else {
-            completed(Result.failure(NetatmoError.noRefreshToken))
-            return
-        }
-        
-        // If the token has not yet expired, just return success
-        if expires < Date(), let authResult = authResultBuilder {
-            completed(Result.success(authResult))
-        }
-        
-        guard let url = URL(string: "https://api.netatmo.com/oauth2/token") else {
-            completed(Result.failure(NetatmoError.badURL))
-            return
-        }
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-        
-        let body = "grant_type=refresh_token&client_id=\(clientId)&client_secret=\(clientSecret)&refresh_token=\(refreshToken)"
-        urlRequest.httpBody = body.data(using: .utf8)!
-        
-        let requestDate = Date()
-        let downloadTask = URLSession.shared.dataTask(with: urlRequest) { [weak self] (data, _, error) in
-            
-            if let error = error {
-                completed(Result.failure(error))
-                return
-            }
-            
-            guard let data = data else {
-                completed(Result.failure(NetatmoError.noData))
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            let result: AuthResult?
-            do {
-                result = try decoder.decode(AuthResult.self, from: data)
-            } catch {
-                completed(Result.failure(error))
-                return
-            }
-            
-            guard let authResult = result, let self = self else {
-                completed(Result.failure(NetatmoError.generalError))
-                return
-            }
-            
-            self.accessToken = authResult.accessToken
-            self.refreshToken = authResult.refreshToken
-            self.expires = authResult.expiresInDate(from: requestDate)
-            
-            completed(Result.success(authResult))
-        }
-        downloadTask.resume()
-    }
-    
-    // MARK: - Auth State Changed Listner
-    
-    func addAuthStateDidChangeListener(_ callback: @escaping ((_ authState: AuthState) -> Void)) -> UUID {
-        // FIXME: Add to queue to avail race conditions
-        let uuid = UUID()
-        authStateDidChangeListeners[uuid] = callback
-        
-        // TODO: Perform callback with current state
-        
-        return uuid
-    }
-    
-    func removeAuthStateDidChangeListener(with uuid: UUID) {
-        // FIXME: Add to queue to avail race conditions
-        authStateDidChangeListeners.removeValue(forKey: uuid)
     }
     
 }
--- NetatmoSwiftAPI/Manager/NetatmoManager.swift
+++ NetatmoSwiftAPI/Manager/NetatmoManager.swift
@@ -8,9 +8,27 @@
 
 import Foundation
 
+/*
+ Workflow notes:
+ 1. Check for UUID in UserDefaults
+   a) If found, go to step 2
+   b) If not found then create UUID, save to UserDefaults and go to step 3
+ 2. Fetch tokens from keychain with UUID as account
+   a) If found and valid, go to step 7
+   b) If not found or invalid, go to step 3
+ 3. Create auth URL with UUID as state
+ 4. Call URL in Safari or WebView
+ 5. Auth callback
+ 6. Save tokens to keychain with UUID as account
+ 7. Auth complete
+*/
+
 public class NetatmoManager {
     
     public static var shared = NetatmoManager()
+    static let userDefaultsSuiteName = "com.PigonaHill.NetatmoSwiftAPI.userDefaults.suiteName"
+    static let userDefaultsKeychainStateUUID = "com.PigonaHill.NetatmoSwiftAPI.userDefaults.stateUUID"
+    static let keychainServiceName = "com.PigonaHill.NetatmoSwiftAPI.keychain"
     
     // MARK: - Types
 
@@ -22,15 +40,30 @@ public class NetatmoManager {
         case noAccessToken
         case noCallbackCode
         case noScope
-        case existingState
         case stateMismatch
     }
 
-    public enum AuthState {
+    public enum AuthState: Equatable {
         case unknown
         case authorized
         case tokenExpired
         case failed(_ error: Error)
+        
+        public static func == (lhs: NetatmoManager.AuthState, rhs: NetatmoManager.AuthState) -> Bool {
+            
+            switch (lhs, rhs) {
+            case (.unknown, .unknown):
+                return true
+            case (.authorized, .authorized):
+                return true
+            case (.tokenExpired, .tokenExpired):
+                return true
+            case (let .failed(lhError), let .failed(rhError)):
+                return lhError.localizedDescription == rhError.localizedDescription
+            default:
+                return false
+            }
+        }
     }
     
     // MARK: - Properties
@@ -43,24 +76,94 @@ public class NetatmoManager {
     var expires: Date?
     var requestedScope: [AuthScope]?
     var stateUUID: String?
-    var authResultBuilder: AuthResult? {
-        
-        guard let accessToken = self.accessToken, let refreshToken = self.refreshToken, let expires = self.expires else {
-            return nil
+    var authState: AuthState = .unknown {
+        didSet {
+            guard oldValue != authState else { return }
+            authStateDidChangeListeners.values.forEach { $0(authState) }
         }
-        return AuthResult(accessToken: accessToken, refreshToken: refreshToken, exires: expires)
     }
     lazy var authStateDidChangeListeners = [UUID: ((authState: AuthState) -> Void)]()
     
     // MARK: - Lifecycle
+    
+    private init() { }
     
     public static func configure(clientId: String, clientSecret: String, redirectURI: String) {
         
         shared.clientId = clientId
         shared.clientSecret = clientSecret
         shared.redirectURI = redirectURI
+        
+        guard let stateUUID = shared.loadStateUUID() else {
+            shared.authState = .unknown
+            return
+        }
+        
+        shared.stateUUID = stateUUID
+        
+        guard let keychainAuthState = try? KeychainPasswordItem(service: NetatmoManager.keychainServiceName, account: stateUUID).readObject() as OAuthState else {
+            shared.authState = .unknown
+            return
+        }
+        
+        guard keychainAuthState.isValid == true else {
+            shared.authState = .unknown
+            
+            // Attempt tokenn refresh
+            shared.refreshToken { (result) in
+                
+                let authState: AuthState
+                switch result {
+                case .success:
+                    authState = .authorized
+                case .failure(let error):
+                    authState = .failed(error)
+                }
+                
+                shared.authState = authState
+            }
+            
+            return
+        }
+        
+        shared.accessToken = keychainAuthState.accessToken
+        shared.refreshToken = keychainAuthState.refreshToken
+        shared.expires = keychainAuthState.expires
+        
+        shared.authState = .authorized
     }
     
-    init() { }
+    // MARK: - State UUID
+    
+    func loadStateUUID() -> String? {
+        
+        guard let userDefaults = UserDefaults(suiteName: NetatmoManager.userDefaultsSuiteName) else {
+            return nil
+        }
+        return userDefaults.object(forKey: NetatmoManager.userDefaultsKeychainStateUUID) as? String
+    }
+    
+    @discardableResult
+    func saveStateUUID(_ uuid: String) -> Bool {
+        
+        guard let userDefaults = UserDefaults(suiteName: NetatmoManager.userDefaultsSuiteName) else {
+            return false
+        }
+        
+        userDefaults.set(uuid, forKey: NetatmoManager.userDefaultsKeychainStateUUID)
+        userDefaults.synchronize()
+        return true
+    }
+    
+    @discardableResult
+    func removeStateUUID() -> Bool {
+        
+        guard let userDefaults = UserDefaults(suiteName: NetatmoManager.userDefaultsSuiteName) else {
+            return false
+        }
+        userDefaults.removeObject(forKey: NetatmoManager.userDefaultsKeychainStateUUID)
+        userDefaults.synchronize()
+        return true
+    }
     
 }
