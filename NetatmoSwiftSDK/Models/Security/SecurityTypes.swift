5c2244268a3fb739aea65e0bb83c49273505b48d
James Barrow <baza207@me.com>
2020-02-24 20:53:28 +0100

    Renamed API -> SDK

--- NetatmoSwiftSDK/Models/Security/SecurityTypes.swift
+++ NetatmoSwiftSDK/Models/Security/SecurityTypes.swift
@@ -1,6 +1,6 @@
 //
 //  SecurityTypes.swift
-//  NetatmoSwiftAPI
+//  NetatmoSwiftSDK
 //
 //  Created by James Barrow on 2020-02-23.
 //  Copyright © 2020 Pig on a Hill Productions. All rights reserved.
